8f6a9c299773a9ad83ddc8e2b74a6a248451591a
JoeyHou <peterzhaoyi@163.com>
2018-07-25 14:26:34 +0800

    Finished Lab5

--- routes/add.js
+++ routes/add.js
@@ -2,5 +2,12 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
-	console.log('hi');
+	var new_json = {
+		"name": req.query.name,
+		"description": req.query.description,
+		"imageURL": 'http://lorempixel.com/400/400/people'
+	};
+	data.friends.push(new_json);
+	// console.log(new_json);
+	res.render('index', data);
  }
